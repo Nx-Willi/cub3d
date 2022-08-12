4d0445f06b8920f05d42f0c0e84b1e9e41cd9882
XavierLB <xavier.lebaux@gmail.com>
2022-08-12 11:34:57 +0200

    Start minimap

--- srcs/main.c
+++ srcs/main.c
@@ -6,57 +6,57 @@
 /*   By: xle-baux <xle-baux@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/04 16:09:24 by wdebotte          #+#    #+#             */
-/*   Updated: 2022/08/10 18:15:18 by xle-baux         ###   ########.fr       */
+/*   Updated: 2022/08/12 11:33:00 by xle-baux         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 
 //__>TEMPORARY_________________________________________________________________
-static void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
-{
-	char	*dst;
+// static void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
+// {
+// 	char	*dst;
 
-	dst = data->addr + (y * data->line_length + x * (data->bits_per_pixel / 8));
-	*(unsigned int *)dst = color;
-}
+// 	dst = data->addr + (y * data->line_length + x * (data->bits_per_pixel / 8));
+// 	*(unsigned int *)dst = color;
+// }
 
-static void	draw_shit(t_mlx *mlx)
-{
-	int	x;
-	int	y;
+// static void	draw_shit(t_mlx *mlx)
+// {
+// 	int	x;
+// 	int	y;
 
-	x = mlx->win_width / 2 - 250;
-	y = mlx->win_heigth / 2 - 250;
-	while (x <= mlx->win_width / 2 + 250)
-		my_mlx_pixel_put(&mlx->img, x++, y, 0x00FFFFFF);
-	while (y <= mlx->win_heigth / 2 + 250)
-		my_mlx_pixel_put(&mlx->img, x, y++, 0x00FFFFFF);
-	while (x >= mlx->win_width / 2 - 250)
-		my_mlx_pixel_put(&mlx->img, x--, y, 0x00FFFFFF);
-	while (y >= mlx->win_heigth / 2 - 250)
-		my_mlx_pixel_put(&mlx->img, x, y--, 0x00FFFFFF);
-	x = mlx->win_width / 2;
-	y = mlx->win_heigth / 2 - 50;
-	while (y <= mlx->win_heigth / 2 + 50)
-		my_mlx_pixel_put(&mlx->img, x, y++, 0x00FFFFFF);
-	x = mlx->win_width / 2 - 50;
-	y = mlx->win_heigth / 2;
-	while (x <= mlx->win_width / 2 + 50)
-		my_mlx_pixel_put(&mlx->img, x++, y, 0x00FFFFFF);
-}
+// 	x = mlx->win_width / 2 - 250;
+// 	y = mlx->win_heigth / 2 - 250;
+// 	while (x <= mlx->win_width / 2 + 250)
+// 		my_mlx_pixel_put(&mlx->img, x++, y, 0x00FFFFFF);
+// 	while (y <= mlx->win_heigth / 2 + 250)
+// 		my_mlx_pixel_put(&mlx->img, x, y++, 0x00FFFFFF);
+// 	while (x >= mlx->win_width / 2 - 250)
+// 		my_mlx_pixel_put(&mlx->img, x--, y, 0x00FFFFFF);
+// 	while (y >= mlx->win_heigth / 2 - 250)
+// 		my_mlx_pixel_put(&mlx->img, x, y--, 0x00FFFFFF);
+// 	x = mlx->win_width / 2;
+// 	y = mlx->win_heigth / 2 - 50;
+// 	while (y <= mlx->win_heigth / 2 + 50)
+// 		my_mlx_pixel_put(&mlx->img, x, y++, 0x00FFFFFF);
+// 	x = mlx->win_width / 2 - 50;
+// 	y = mlx->win_heigth / 2;
+// 	while (x <= mlx->win_width / 2 + 50)
+// 		my_mlx_pixel_put(&mlx->img, x++, y, 0x00FFFFFF);
+// }
 
-static void	draw_point(t_info *infos)
-{
-	t_mlx	*mlx;
+// static void	draw_point(t_info *infos)
+// {
+// 	t_mlx	*mlx;
 
-	mlx = &infos->mlx;
-	mlx->img.img = mlx_new_image(mlx->mlx, mlx->win_width, mlx->win_heigth);
-	mlx->img.addr = mlx_get_data_addr(mlx->img.img, &mlx->img.bits_per_pixel,
-			&mlx->img.line_length, &mlx->img.endian);
-	draw_shit(mlx);
-	mlx_put_image_to_window(mlx->mlx, mlx->window, mlx->img.img, 0, 0);
-}
+// 	mlx = &infos->mlx;
+// 	mlx->img.img = mlx_new_image(mlx->mlx, mlx->win_width, mlx->win_heigth);
+// 	mlx->img.addr = mlx_get_data_addr(mlx->img.img, &mlx->img.bits_per_pixel,
+// 			&mlx->img.line_length, &mlx->img.endian);
+// 	draw_shit(mlx);
+// 	mlx_put_image_to_window(mlx->mlx, mlx->window, mlx->img.img, 0, 0);
+// }
 //‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾
 
 static void	init_mlx(t_info *infos)
--- srcs/minimap.c
+++ srcs/minimap.c
@@ -6,7 +6,7 @@
 /*   By: xle-baux <xle-baux@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/11 15:19:58 by xle-baux          #+#    #+#             */
-/*   Updated: 2022/08/11 17:14:57 by xle-baux         ###   ########.fr       */
+/*   Updated: 2022/08/12 11:34:44 by xle-baux         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,15 +20,43 @@ static void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
 	*(unsigned int *)dst = color;
 }
 
+static void	draw_cube(t_mlx *mlx, int start_x, int start_y)
+{
+	int	x;
+	int	y;
+
+	x = start_x;
+	y = start_y;
+	while (y >= start_y - 10)
+	{
+		printf("toto");
+		x = start_x;
+		while (x <= start_x + 10)
+			my_mlx_pixel_put(&mlx->img, x++, y, 0x00FFFFFF);
+		y--;
+	}
+}
+
 void	minimap(t_info *infos)
 {
-	t_mlx mlx;
+	int		start_x;
+	int		start_y;
+	int		x;
+	int		y;
+	t_mlx	*mlx;
 
+	start_x = 0;
+	start_y = infos->mlx.win_heigth - 100;
+	x = 0;
+	y = -1;
 	mlx = &infos->mlx;
 	mlx->img.img = mlx_new_image(mlx->mlx, mlx->win_width, mlx->win_heigth);
 	mlx->img.addr = mlx_get_data_addr(mlx->img.img, &mlx->img.bits_per_pixel,
 			&mlx->img.line_length, &mlx->img.endian);
-
+	while (infos->i_map[++y])
+	{
+		while (infos->i_map[y][x++])
+			draw_cube(mlx, start_x + (x * 10), start_y - (y * 10));
+	}
 	mlx_put_image_to_window(mlx->mlx, mlx->window, mlx->img.img, 0, 0);
-
 }
